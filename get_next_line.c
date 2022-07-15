9570fc9e3035dd7243ec1b0fd96cad614d32d60d
Mahmoud Khalil <m94khalil@gmail.com>
2022-07-15 20:43:43 +0400

    started

--- get_next_line.c
+++ get_next_line.c
@@ -1,1 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkhalil <mkhalil@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 22:35:11 by mkhalil           #+#    #+#             */
+/*   Updated: 2022/07/15 20:41:18 by mkhalil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	*buff;
+	int		size;
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	size = read(fd, buff, BUFFER_SIZE);
+	if (size == -1)
+		return (NULL);
+	if (size < BUFFER_SIZE)
+		buff[size] = '\0';
+	i = 0;
+	while (buff[i])
+	{
+		if (buff[i] == '\n')
+		
+	}
+}
