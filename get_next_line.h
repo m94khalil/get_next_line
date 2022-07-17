56e92964895966d28ff727044bb71ebac83e608b
Mahmoud Khalil <m94khalil@gmail.com>
2022-07-17 13:39:27 +0400

    almost

--- get_next_line.h
+++ get_next_line.h
@@ -16,7 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
-#include <stdio.h>
+# include <stdio.h>
 
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *str, int c);
